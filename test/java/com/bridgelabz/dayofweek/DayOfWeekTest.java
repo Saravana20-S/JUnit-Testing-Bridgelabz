1aa18ab4705835d04a470e5741bc22447b8ffa3f
Saravana20-S <iamsaravanan1347@gmail.com>
2026-06-22 15:26:59 +0530

    feat(Junit-Testing): DAYOFWEEK test Logic added

---
+++ test/java/com/bridgelabz/dayofweek/DayOfWeekTest.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.dayofweek;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DayOfWeekTest {
+
+    @Test
+    void testSunday() {
+        assertEquals(0,
+                DayOfWeek.dayOfWeek(6, 21, 2026));
+    }
+
+    @Test
+    void testMonday() {
+        assertEquals(1,
+                DayOfWeek.dayOfWeek(6, 22, 2026));
+    }
+
+    @Test
+    void testTuesday() {
+        assertEquals(2,
+                DayOfWeek.dayOfWeek(6, 23, 2026));
+    }
+
+    @Test
+    void testNewYear2025() {
+        assertEquals(3,
+                DayOfWeek.dayOfWeek(1, 1, 2025));
+    }
+
+    @Test
+    void testLeapYearDate() {
+        assertEquals(4,
+                DayOfWeek.dayOfWeek(2, 29, 2024));
+    }
+}
